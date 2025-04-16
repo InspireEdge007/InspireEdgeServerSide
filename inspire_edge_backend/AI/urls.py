b241c4789ba2e7d78e73f3bab2b331c57f1e8c1c
unknown <divine41744@gmail.com>
2025-04-16 14:11:00 +0100

    added enabled debug to true

--- inspire_edge_backend/AI/urls.py
+++ inspire_edge_backend/AI/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('customauth', include("customauth.urls")),
+    path('customauth/', include("customauth.urls")),
 
 ]
