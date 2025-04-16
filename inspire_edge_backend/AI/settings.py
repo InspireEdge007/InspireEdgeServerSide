636e9342b3a03c04da9313f714c0c1e62cab9ecb
unknown <divine41744@gmail.com>
2025-04-16 13:38:56 +0100

    added docker and settings.py

--- inspire_edge_backend/AI/settings.py
+++ inspire_edge_backend/AI/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -111,6 +112,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = "static/"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
