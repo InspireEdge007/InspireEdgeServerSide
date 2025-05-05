25d7940fc183df291a3e8bd107415c6030ab6225
unknown <divine41744@gmail.com>
2025-05-05 19:59:08 +0100

    added a new endpoint for fetch

--- inspire_edge_backend/shopify_integration/urls.py
+++ inspire_edge_backend/shopify_integration/urls.py
@@ -1,11 +1,11 @@
 # shopify_integration/urls.py
 
 from django.urls import path
-from .views import ShopifyAuthRedirectView, ShopifyCallbackView, CompareProductsView
+from .views import ShopifyAuthRedirectView, ShopifyCallbackView, CompareProductsView, FetchProductsView
 
 urlpatterns = [
     path('auth', ShopifyAuthRedirectView.as_view(), name='shopify-auth'),
     path('callback', ShopifyCallbackView.as_view(), name='shopify-callback'),
-    path('fetch-products', ShopifyCallbackView.as_view(), name='shopify-products'),
+    path('fetch-products', FetchProductsView.as_view(), name='shopify-products'),
     path('compare', CompareProductsView.as_view(), name='shopify-compare'),
 ]
